cf3ea133b10e5e9ac5ca7025ff5ff10eb190b2bb
Akamya <anthonyshc.contact@gmail.com>
2023-01-03 11:37:02 +0100

    Added rotation detection

--- js/app.js
+++ js/app.js
@@ -1,29 +1,35 @@
 window.onload = () => {
     if('geolocation' in navigator) {
-        let userGeoID = navigator.geolocation.watchPosition((pos) => {
+        navigator.geolocation.watchPosition((pos) => {
             updateCoords(pos.coords.longitude, pos.coords.latitude, pos.coords.altitude);
             updateInfos(pos.coords.accuracy, pos.coords.speed, pos.timestamp);
         });
-        // navigator.geolocation.getCurrentPosition((pos) => {
-        //     // Callback to update the coords
-        //     updateCoords(pos.coords.longitude, pos.coords.latitude, pos.coords.altitude);
-        //     updateInfos();
-        // });
     } else {
         // If browser refused Geolocation, print error
         console.log("Geolocation not available on this browser");
     }
+
+    window.addEventListener("deviceorientation", updateDeviceRotationFields, true);
+
 }
 
 // Basic function to update the DOM HTML elements
 function updateCoords(long, lat, alt) {
-    document.getElementById("long").innerHTML = long.toString();
-    document.getElementById("lat").innerHTML = lat.toString();
-    document.getElementById("alti").innerHTML = alt.toString();
+    document.getElementById("long").innerHTML = long == null ? "No data" : long.toString();
+    document.getElementById("lat").innerHTML = lat == null ? "No data" : lat.toString();
+    document.getElementById("alti").innerHTML = alt == null ? "No data" : alt.toString();
 }
 
 function updateInfos(acc, speed, date) {
-    document.getElementById("accuracy").innerHTML = acc.toString();
-    document.getElementById("speed").innerHTML = speed.toString();
-    document.getElementById("current-date").innerHTML = date.toString();
+    document.querySelector(".accuracy").innerHTML = acc == null ? "No data" : acc.toString();
+    document.querySelector(".speed").innerHTML = speed == null ? "No data" : speed.toString();
+    document.querySelector(".current-date").innerHTML = date == null ? "No data" : new Date(date);
+}
+
+function updateDeviceRotationFields(e) {
+    let alpha = e.alpha;
+    let beta = e.beta;
+    let gamma = e.gamma;
+
+    document.querySelector(".alpha").innerHTML = alpha;
 }
