4091f86a2cfc4e8ecfb6ef285c9d9abc83ec183d
Akamya <anthonyshc.contact@gmail.com>
2023-01-03 11:09:41 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 window.onload = () => {
     if('geolocation' in navigator) {
         let userGeoID = navigator.geolocation.watchPosition((pos) => {
-            console.log(pos);
+            updateCoords(pos.coords.longitude, pos.coords.latitude, pos.coords.altitude);
+            updateInfos(pos.coords.accuracy, pos.coords.speed, pos.timestamp);
         });
-        console.log(userGeoID);
         // navigator.geolocation.getCurrentPosition((pos) => {
         //     // Callback to update the coords
         //     updateCoords(pos.coords.longitude, pos.coords.latitude, pos.coords.altitude);
@@ -17,7 +17,13 @@ window.onload = () => {
 
 // Basic function to update the DOM HTML elements
 function updateCoords(long, lat, alt) {
-    document.getElementById("long").innerHTML =long.toString();
+    document.getElementById("long").innerHTML = long.toString();
     document.getElementById("lat").innerHTML = lat.toString();
     document.getElementById("alti").innerHTML = alt.toString();
+}
+
+function updateInfos(acc, speed, date) {
+    document.getElementById("accuracy").innerHTML = acc.toString();
+    document.getElementById("speed").innerHTML = speed.toString();
+    document.getElementById("current-date").innerHTML = date.toString();
 }
