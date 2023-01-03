8dcc6ee2ab8ceeac4d7309a13b2b61b07933a69c
Akamya <anthonyshc.contact@gmail.com>
2023-01-03 09:48:27 +0100

    Uptaded js and html

--- js/app.js
+++ js/app.js
@@ -1,16 +1,9 @@
 window.onload = () => {
     if('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition((pos) => {
-            uptadeHtml(pos.coords.longitude, pos.coords.latitude, pos.coords.altitude);
+            document.getElementById("long").innerHTML = pos.coords.longitude;
         });
     } else {
         console.log("Geolocation not available on this browser")
     }
-}
-
-function uptadeHtml(long, lat, alt) {
-    let elemList = [".long", ".lat", ".alti"];
-    elemList.forEach(e => {
-        document.querySelector(e).innerHTML = long;
-    })
 }
