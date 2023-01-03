ce120dac0102b815c55a1a4b8e55f39256c70abc
Akamya <anthonyshc.contact@gmail.com>
2023-01-03 09:37:13 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 window.onload = () => {
     if('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition((pos) => {
-            document.querySelector("long").innerHTML = toString(pos.coords.longitude);
+            document.querySelector("long").innerHTML = pos.coords.longitude;
         })
     } else {
         console.log("Geolocation not available on this browser")
