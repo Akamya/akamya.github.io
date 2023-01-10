1d23643b7b3584613efb9654a5557a0b009f018d
Akamya <anthonyshc.contact@gmail.com>
2023-01-10 11:03:39 +0100

    Update app.js
    Added the bermudes triangle as well as changed the maps tiles

--- leaflet/app.js
+++ leaflet/app.js
@@ -1,15 +1,23 @@
 window.onload = () => {
     var map = L.map('map').fitWorld();
     var niceCentre = [43.7102, 7.2620];
+    var bermudeTriangle = [
+        [32.99, -64.79],
+        [25.761681, -80.191788],
+        [18.46633, -66.10572]
+    ];
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
+
+    L.tileLayer('http://stamen-tiles-a.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
+        maxZoom: 18,
         attribution: '© OpenStreetMap'
     }).addTo(map);
 
     map.locate({setView: true, maxZoom: 16});
 
     L.marker(niceCentre).addTo(map);
+    L.polygon(bermudeTriangle).addTo(map);
+
 
     function onLocationFound(e) {
         var radius = e.accuracy;
